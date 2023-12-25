a0975b3d30f379fda4b7636883999eba737f1164
shkar <shkarhsardar@gmail.com>
2023-12-25 12:52:48 +0300

    finish auth

---
+++ app/(auth)/(routes)/signin/[[...signin]]/page.tsx
@@ -0,0 +1,11 @@
+
+import { SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+const SiginIn = () => {
+  return (
+    <SignUp />
+  )
+}
+
+export default SiginIn
---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <p className="text-3xl font-medium text-sky-700">
+        Hello
+      </p>
+      <Button variant="destructive">Click Here!</Button>
+    </div>
+  )
+}
+
+export default Home
