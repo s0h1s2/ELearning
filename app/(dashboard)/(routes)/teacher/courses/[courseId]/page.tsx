7624120efbb4bd6de5b25835ff0cd49977864fb9
shkar <shkarhsardar@gmail.com>
2024-01-04 18:30:41 +0300

    added view course page

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,46 @@
+import { db } from '@/lib/db'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation';
+import React from 'react'
+
+const CourseId = async ({ params }: { params: { courseId: string } }) => {
+  const userId = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId
+    }
+  })
+  if (!course) {
+    return redirect("/");
+  }
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.thumbnail,
+    course.price,
+    course.categoryId,
+  ]
+  const totalFields = requiredFields.length
+  const completedFields = requiredFields.filter(Boolean).length
+  const completionText = `(${completedFields}/${totalFields})`;
+
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">
+            Course setup
+          </h1>
+          <span>
+            Complete all fields {completionText}
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CourseId 
