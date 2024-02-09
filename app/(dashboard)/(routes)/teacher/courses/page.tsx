812fc7a15748ca9b1f69003e361664cdc733d0f3
shkar <shkarhsardar@gmail.com>
2024-02-09 17:47:40 +0300

    added table for teacher page

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,8 +1,26 @@
 import { Button } from '@/components/ui/button'
+import { db } from '@/lib/db'
+import { auth } from '@clerk/nextjs'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 import React from 'react'
+import { DataTable } from './_components/DataTable'
+import { columns } from './_components/Columns'
 
-const TeacherCourses = () => {
+const TeacherCourses = async () => {
+  const { userId } = auth()
+  if (!userId) {
+    return redirect("/")
+  }
+  const courses = await db.course.findMany({
+    where: {
+      userId: userId
+    },
+    orderBy: {
+      createdAt: "desc"
+    }
+  })
+
   return (
     <div className="p-6">
       <Link href="/teacher/create">
@@ -10,6 +28,7 @@ const TeacherCourses = () => {
           New Course
         </Button>
       </Link>
+      <DataTable data={courses} columns={columns} />
     </div>
   )
 }
