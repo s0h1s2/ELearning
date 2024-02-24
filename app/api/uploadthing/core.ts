ff7b87765c0285b01ba85454f0ca01bd1cf45c79
shkar <shkarhsardarm@gmail.com>
2024-02-24 18:27:53 +0300

    fix a couple of problems

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -4,17 +4,22 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 const f = createUploadthing();
 
 const handleAuth = () => {
-  const { userId } = auth()
-  if (!userId) {
-    throw new Error("Unauthorized user")
-  }
-  return userId
-}
-export const ourFileRouter = {
-  courseImage: f({ image: { maxFileSize: "1MB", maxFileCount: 1 } }).middleware(() => handleAuth()).onUploadComplete(() => { }),
-  courseAttatchment: f(["text", "image", "pdf"]).middleware(() => handleAuth()).onUploadComplete(() => { }),
-  chapterVideo: f({ video: { maxFileCount: 1, maxFileSize: "512GB" } }).middleware(() => handleAuth()).onUploadComplete(() => { })
+  const { userId } = auth();
 
+  if (!userId) throw new Error("Unauthorized");
+  return { userId };
+}
+
+export const ourFileRouter = {
+  courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => { }),
+  courseAttatchment: f(["text", "image", "video", "audio", "pdf"])
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => { }),
+  chapterVideo: f({ video: { maxFileCount: 1, maxFileSize: "512GB" } })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => { })
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
